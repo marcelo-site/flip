5de900d837ce63290d422684f13f8baee06c4aac
Marcelo <57197501+marcelo-site@users.noreply.github.com>
2023-08-28 19:55:37 -0300

    add pdf

---
+++ src/assets/js/cart.js
@@ -0,0 +1,91 @@
+if (!localStorage.hasOwnProperty('cart')) {
+    localStorage.setItem('cart', JSON.stringify([]))
+}
+
+const orderExists = JSON.parse(localStorage.getItem('cart'))
+const content = document.querySelector('#content')
+const totCart = document.querySelector('#total')
+
+const insertContent = async () => {
+    if (orderExists.length > 0) {
+        try {
+            await Promise.all(orderExists.map(async (order, i) => {
+                const divContainer = document.createElement('div')
+                divContainer.classList.add('container')
+                const div = document.createElement('div')
+                div.classList.add('front')
+                const divImg = document.createElement('div')
+                const img = document.createElement('img')
+                const urlImg = order.img
+                const myBlob = await fetch(urlImg)
+                if (!myBlob.ok) {
+                    throw new Error('Imagem ausente!')
+                }
+                const srcImg = await myBlob.blob()
+                img.src = URL.createObjectURL(srcImg);
+                divImg.append(img)
+                div.append(divImg)
+                divInfo = document.createElement('div')
+                divInfo.classList.add('info')
+                const info = order.desc.split('-')
+                const title = document.createElement('h2')
+                title.innerHTML = info[2]
+                divInfo.append(title)
+
+                const cor = document.createElement('h3')
+                cor.innerHTML = info[1]
+                divInfo.append(cor)
+                const price = document.createElement('p')
+                price.innerHTML = `<span class="bold">Preço: </span> ${order.price}`
+                divInfo.append(price)
+                const sizeQty = document.createElement('p')
+                sizeQty.innerHTML = `<span class="bold">Tamanho: </span>${order.size} <span class="bold">Quant:</span> ${order.qty}`
+                divInfo.append(sizeQty)
+                div.append(divInfo)
+                const subTotal = document.createElement('p')
+                price.classList.add('price')
+                subTotal.innerHTML = `<span class="bold">Subtotal: </span> <span class="subtotal">${parseFloat(order.price) * parseInt(order.qty)}</span>`
+                divInfo.append(subTotal)
+                divContainer.append(div)
+                content.append(divContainer)
+                return
+            }))
+        } catch (error) {
+            console.log(error)
+        }
+        setTimeout(() => {
+            const container = Array.from(document.querySelectorAll('.container'))
+            const sizeImg = document.querySelector('.container img').clientHeight
+            container.map(el => {
+                el.style.height = `${parseInt(sizeImg) + 150}px`
+            })
+            const total = Array.from(document.querySelectorAll('.subtotal'))
+                .reduce((acc, cur) => acc + parseFloat(cur.innerHTML), 0)
+            document.querySelector('#total').innerHTML = `<span class="bold">O total da sua sacola é:</span> ${total.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}`
+        }, 300)
+    } else {
+        const div = document.createElement('div')
+        div.innerHTML = 'Não Há produtos no momento!'
+        content.append(div)
+    }
+}
+
+// gerar pdf
+const generetePDF = document.querySelector('#pdf')
+
+generetePDF.addEventListener('click', () => {
+    const options = {
+        margin: 1,
+        filename: "arquivo.pdf",
+        html2canvas: { sacle: 2 },
+        pagebreak: {
+            //  before: '.beforeClass', after: ['#after1', '#after2'],
+         avoid: '.container' },
+        image: { type: 'jpeg', quality: 0.98 },
+        jsPDF: { unit: "mm", format: "a5", orientation: "portrait" }
+    }
+    console.log(content)
+    html2pdf().set(options).from(content).save()
+})
+
+window.addEventListener('load', () => insertContent())
---
+++ src/assets/js/index.js
@@ -0,0 +1,122 @@
+const flip = document.querySelectorAll('.flip')
+const fronts = document.querySelectorAll('.front .btn')
+const versos = document.querySelectorAll('.verso .virate .btn')
+const divVersos = document.querySelectorAll('.verso-products')
+const bulets = document.querySelectorAll('.bulets span')
+const divBultests = document.querySelectorAll('.bulets')
+const labels = document.querySelectorAll('form .info .sizes label')
+const inputsSize = document.querySelectorAll('form .info input[type="radio"]')
+const forms = document.querySelectorAll('.verso-products form')
+let width = window.innerWidth
+const msg = document.querySelector('#msg')
+const cartQty = document.querySelector('#cart-qty')
+const prices = document.querySelectorAll('.price')
+const bulets1 = document.querySelectorAll('#bulets-1 span div')
+const bulets2 = document.querySelectorAll('#bulets-2 span div')
+
+if(!localStorage.hasOwnProperty('cart')) {
+    localStorage.setItem('cart', JSON.stringify([]))
+}
+
+const markBuletPosition = (paramPositon) => {
+    const bulets = divBultests[paramPositon].querySelectorAll('span div')
+    bulets.forEach(bulet => bulet.style.backgroundColor = '')
+    const position = divVersos[paramPositon].scrollLeft / (width * 0.88)
+    bulets[parseInt(position)].style.backgroundColor = 'blue'
+}
+
+divVersos.forEach((el, i)=> el.addEventListener('scroll', () => markBuletPosition(i)))
+const orderExists=  JSON.parse(localStorage.getItem('cart'))
+
+inputsSize.forEach((size, i) => {
+    size.addEventListener('click', () => {
+        labels.forEach(label => label.classList.remove('checked'))
+        if (size.checked) {
+            labels[i].classList.add('checked')
+        }
+    })
+})
+
+forms.forEach(form => {
+    form.addEventListener('submit', (e) => {
+        const div = document.createElement('div')
+        div.classList.add('message')
+        try {
+            e.preventDefault()
+            const sizeInput = e.target.size
+            const size = sizeInput.value
+            const desc = Array.from(sizeInput).filter(el => el.checked)
+            if (desc.length === 0) {
+                throw new Error('Escolha um tamanho!')
+            }
+            const img = e.target.img.src
+            const price = e.target.price.value
+            const qty = e.target.qty.value
+            const order = {
+                img, price, size, qty, desc: desc[0].id
+            }
+            if(orderExists) {
+                orderExists.push(order)
+                localStorage.setItem('cart', JSON.stringify(orderExists))
+            } else {
+                localStorage.setItem('cart', JSON.stringify([order]))
+            }
+            div.innerHTML = 'Você acaba de colocar um item na sacola!'
+            div.classList.add('sucess')
+            msg.append(div)
+            const cartQtyTot = parseInt(cartQty.innerHTML)
+            cartQty.innerHTML = cartQtyTot + 1
+        } catch (error) {
+            div.innerHTML = error.message
+            div.classList.add('alert')
+            msg.append(div)
+        } finally {
+            setTimeout(() => msg.removeChild(div), 3000)
+        }
+    })
+})
+
+divVersos.forEach((gridProduct, i) => {
+    const count = divVersos[i].querySelectorAll('.info').length
+    gridProduct.style.gridTemplateColumns = `repeat(${count}, 1fr)`;
+})
+
+versos.forEach((el, i) => {
+    el.addEventListener('click', () => flip[i].style.transform = '')
+})
+
+fronts.forEach((el, i) => {
+    el.addEventListener('click', () => {
+        flip[i].style.transform = 'rotateY(180deg)'
+        changeCarrosel()
+    })
+})
+
+divVersos.forEach((verso, i) => divBultests[i].setAttribute('data-index-products', i))
+
+const changeCarrosel = () => {
+    bulets.forEach((bulet, i) => {
+        bulet.addEventListener('click', (el) => {
+            const parent = bulet.parentNode
+            const index = parent.getAttribute('data-index-products')
+            const lengthBuletAtual = parent.querySelectorAll('span')
+            lengthBuletAtual.forEach((elem, ind) => {
+                elem.addEventListener('click', () => {
+                    divVersos[index].scrollLeft = width * ind + 20
+                })
+            })
+        })
+    })
+}
+
+window.addEventListener('load', () => {
+    if(orderExists.length > 0) {
+        cartQty.innerHTML= orderExists.length
+    }
+    changeCarrosel()
+    const container = document.querySelectorAll('.container')
+    const sizeImg = document.querySelector('.container img')
+    container.forEach(el => {
+        el.style.height = `${sizeImg.clientHeight + 220}px`
+    })
+})
